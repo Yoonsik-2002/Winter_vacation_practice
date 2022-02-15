816e09af1be7dd7d1e6f21fc4dbaa9ec7f02452c
Yoonsik-2002 <yoonsikp119105@gmail.com>
2022-02-15 15:43:54 +0900

    11-2_문제2(Solution2)
    int 형 변수 num1과 num2를 선언과 동시에 각각 10과 20으로 초기화하고, int형 포인터 변수 ptr1과 ptr2를 선언하여 각각 num1과 num2를 가리키게 하자. 그리고 이 상태에서 포인터 변수 ptr1과 ptr2를 이용해서 num1의 값을 10 증가시키고, num2의 값을 10 감소시키자. 이제 두 포인터 변수 ptr1과 ptr2가 가리키는 대상을 서로 바꾸자. 즉, 포인터 변수 ptr1이 num2를 가리키게 하고, 포인터 변수 ptr2가 num1을 가리키게 하자. 그리고 마지막으로 ptr1과 ptr2가 가리키는 변수에 저장된 값을 출력하자.
    
    1) 포인터 변수 선언을 통해 ptr1은 num1의 주소값을, ptr2는 num2의 주소값을 저장하게 하였다.
    2) (*ptr1) += 10;, (*ptr2) -=10; 을 이용하여 num1은 10 증가시키고, num2는 10감소시켰다.
    3)  먼저, temp = ptr1; 을 이용하여 temp가 num1을 가리키게 한 뒤, ptr1 = ptr2; 를 이용하여 ptr1이 num2를 가리키게 하였고 ptr2 = temp; 를 이용하여 ptr2가 num1을 가리키게 함으로써 ptr1과 ptr2가 가리키는 대상을 서로 바꾸었다.

--- WinterVacationPractice/WinterVacationPractice/ChallengePointer_2.c
+++ WinterVacationPractice/WinterVacationPractice/ChallengePointer_2.c
@@ -2,21 +2,22 @@
 
 int main(void)
 {
-	int num1 = 10; 
-	int num2 = 20;  
-	int num0 = 0;
+	int num1 = 10; int num2 = 20;
 
 	int* ptr1 = &num1;
 	int* ptr2 = &num2;
+	int* temp; // int형 포인터 변수 temp선언
 
-	*ptr1 += 10;
-	*ptr2 -= 10;
+	(* ptr1) += 10;
+	(* ptr2) -= 10;
 
-	ptr1 = &num0;
-	ptr2 = &num1;
-	ptr1 = &num2;
+	temp = ptr1; // 포인터 변수 temp가 ptr1(num1)을 가리킴
+	ptr1 = ptr2;
+	ptr2 = temp;
 
 	printf("ptr1이 가리키는 변수에 저장된 값 : %d --- ptr2가 가리키는 변수에 저장된 값 : %d\n", *ptr1, *ptr2);
 
 	return 0;
+
 }
+
